fa7567d52cc56d18389533981bb50a164816be22
subash kc <59048447+subash9860@users.noreply.github.com>
2020-03-11 22:37:41 +0545

    Add files via upload

---
+++ area of squre.c
@@ -0,0 +1,10 @@
+//2.	Write a program to calculate the surface area of cube. [Hint  area=6L2]
+ #include<stdio.h>
+ int main()
+ {
+ 	int l, area;
+ 	printf("enter the length of the cube");
+ 	scanf("%d",&l);
+ 	area= 6*(l*l);
+ 	printf("area of the squre of the cube is :%d",area);
+ }
