89cf7e6a9545f45e91c02484b9e0441ce683f03f
subash kc <59048447+subash9860@users.noreply.github.com>
2020-03-12 00:14:11 +0545

    Add files via upload

---
+++ octal and hexa.c
@@ -0,0 +1,10 @@
+// 18.	Entered a number in decimal and print it in octal and hexadecimal.
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number ");
+	scanf("%d",&n);
+	printf("octal is %o\n",n);
+	printf("hexa is %x",n);
+}
