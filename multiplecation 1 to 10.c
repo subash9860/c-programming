a05c0c23d1936749f66ee2a1f9a2d019b2642663
subash kc <59048447+subash9860@users.noreply.github.com>
2020-03-13 20:49:50 +0545

    Add files via upload

---
+++ multiplecation 1 to 10.c
@@ -0,0 +1,12 @@
+// 10.	Write a program to display Multiplication table from 1-10.
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=10;i++)
+	{
+		for(j=1;j<=10;j++)
+		printf("%3d*%3d=%3d\n",i,j,i*j);
+		printf("\n");
+}
+}
